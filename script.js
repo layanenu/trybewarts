d3924b884027155208d8dfe373d06c3cf0df4149
Siigmaf <faby.tbe@gmail.com>
2022-07-14 12:09:58 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const form = document.querySelector('.trybewarts-login');
+const btnSubmit = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
 
 form.addEventListener('submit', (e) => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -9,3 +11,9 @@ form.addEventListener('submit', (e) => {
     alert('Email ou senha inválidos.');
   } e.preventDefault();
 });
+
+function enableBtnSubmit() {
+  btnSubmit.disabled = !agreement.checked;
+}
+
+agreement.addEventListener('click', enableBtnSubmit);
